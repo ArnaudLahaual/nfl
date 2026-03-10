aa42351fb36be2db4d8cd40462892f6d1d37bf3c
ArnaudLahaual <arnaud.lahaual@gmail.com>
2026-03-10 22:43:39 +0100

    mis en place du formulaire de connexion

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,3 +1,63 @@
+import React from "react";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Flex, Form, Input } from "antd";
+
 export default function Login() {
-  return <div>Se connecter</div>;
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+  return (
+    <div className="flex flex-col">
+      <div>
+        <h2>Vous avez déjà un compte ? Connectez vous</h2>
+      </div>
+      <div>
+        <Form
+          name="login"
+          initialValues={{ remember: true }}
+          style={{ maxWidth: 360 }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              { required: true, message: "Veuillez renseigner votre pseudo" },
+            ]}
+          >
+            <Input prefix={<UserOutlined />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Veuillez renseigner votre mot de passe",
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Flex justify="space-between" align="center">
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Se souvenir de moi</Checkbox>
+              </Form.Item>
+              <a href="">Mot de passe oublié</a>
+            </Flex>
+          </Form.Item>
+
+          <Form.Item>
+            <Button block type="primary" htmlType="submit">
+              Log in
+            </Button>
+            or <a href="">Register now!</a>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 }
